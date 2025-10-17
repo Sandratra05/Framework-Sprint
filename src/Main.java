d1f8272150ee2c3cb8097afc506cb9750ae49e45
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2025-10-17 16:06:22 +0300

    creation annotation et test de l'annotation dans un main

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.lang.ModuleLayer.Controller;
+
+import annotations.TestUrl;
+import annotations.Url;
+
+public class Main {
+        public static void main(String[] args) throws NoSuchMethodException, SecurityException {
+        // Exemple : on scanne la classe UserController
+        Class<?> clazz = TestUrl.class;
+        
+        if (clazz.getMethod("greeting").isAnnotationPresent(Url.class)) {
+            Url url = clazz.getMethod("greeting").getAnnotation(Url.class);
+            System.out.println("Valeur de l'url : " + url.value());
+            System.out.println("Nom du methode : " + clazz.getMethod("greeting").getName());
+        }
+    }
+}
