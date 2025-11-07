cf8f6c4f9046fc1b4ce88a90e2321ff01b975555
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2025-11-07 22:49:55 +0300

    modification de script de deploiement

--- deploy-jar.sh
+++ deploy-jar.sh
@@ -10,4 +10,8 @@
 
 #cp frameworkServlet.jar /home/zark/Bureau/ITU/Annee-3/Framework/Test/lib/frameworkServlet.jar
 
-cd /home/zark/Bureau/ITU/Annee-3/Framework/Sprint && javac -d bin -cp "lib/*" src/FrontServlet.java src/utils/Scan.java src/annotations/Controller.java src/annotations/Url.java && cd bin && jar cvf frameworkServlet.jar . && cp frameworkServlet.jar /home/zark/Bureau/ITU/Annee-3/Framework/Test/lib/
+cd /home/zark/Bureau/ITU/Annee-3/Framework/Sprint && 
+javac -d bin -cp "lib/*" src/*.java src/utils/*.java src/annotations/*.java src/annotations/*.java && 
+cd bin && 
+jar cvf frameworkServlet.jar . && 
+cp frameworkServlet.jar /home/zark/Bureau/ITU/Annee-3/Framework/Test/lib/
