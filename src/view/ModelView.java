ef61c8c4d8eeb8b28f0501d5a1e4d7efa384ec9e
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2025-11-08 21:42:02 +0300

    Add constructors to ModelView class for initialization

--- src/view/ModelView.java
+++ src/view/ModelView.java
@@ -2,6 +2,13 @@ package view;
 public class ModelView {
     public String view;
 
+    public ModelView() {
+    }
+
+    public ModelView(String view) {
+        setView(view);
+    }
+
     public String getView() {
         return view;
     }
