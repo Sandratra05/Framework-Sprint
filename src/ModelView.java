079979198915c7dadd4d2c2bf503b1960fbbda05
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2025-11-08 21:34:18 +0300

    creation of ModelView

---
+++ src/ModelView.java
@@ -0,0 +1,11 @@
+public class ModelView {
+    public String view;
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+}
