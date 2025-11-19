14d064d7fd730964c47b2a00c5845310254c076e
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2025-11-19 09:24:12 +0300

    renvoi de donnees depuis controller vers vue

--- src/view/ModelView.java
+++ src/view/ModelView.java
@@ -1,12 +1,18 @@
 package view;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModelView {
     public String view;
+    public Map<String, Object> items = new HashMap<>();
 
     public ModelView() {
     }
 
-    public ModelView(String view) {
-        setView(view);
+    public ModelView(String view, Map<String, Object> items) {
+        this.setView(view);
+        this.setItems(items);
     }
 
     public String getView() {
@@ -16,4 +22,16 @@ public class ModelView {
     public void setView(String view) {
         this.view = view;
     }
+
+    public Map<String, Object> getItems() {
+        return items;
+    }
+
+    public void setItems(Map<String, Object> items) {
+        this.items = items;
+    }
+
+    public void addAttribute(String name, Object value) {
+        this.items.put(name, value);
+    }    
 }
